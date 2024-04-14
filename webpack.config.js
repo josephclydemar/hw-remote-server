23a0294575d529a620b2df4f5ae680dac6f19f03
Joseph Clyde Mar <clydemar234@gmail.com>
2024-04-14 22:44:01 +0800

    chore: Add more dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: './temp-build/server.js',
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'commonjs',
+    },
+    target: 'node',
+    mode: 'production',
+    stats: 'errors-warnings',
+    externals: {
+        express: 'express',
+        mongoose: 'mongoose',
+        uuid: 'uuid',
+        dotenv: 'dotenv',
+        socketio: 'socket.io',
+        datefns: 'date-fns'
+    }
+};
