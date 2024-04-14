f5da54e1de4d3b3e09e67005225eb4f1f7ffb71e
Joseph Clyde Mar <clydemar234@gmail.com>
2024-04-15 00:46:17 +0800

    feat: Use the /api/v1/authorized_users route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import path from 'path';
-import fs from 'fs';
 import cors from 'cors';
 
 import dotenv from 'dotenv';
-import { v4 as uuid } from 'uuid';
-import { format } from 'date-fns';
 import mongoose from 'mongoose';
-import express, { Express, Response, Request } from 'express';
-import { Server } from 'socket.io';
+import express, { Express, Response, Request, NextFunction } from 'express';
+// import { Server } from 'socket.io';
+
+import { logEvent } from './middlewares/logger';
 
 dotenv.config();
 
@@ -15,12 +13,28 @@ const PORT: string = process.env.PORT || '8700';
 const NODE_ENV: string = process.env.NODE_ENV || 'production';
 const DATABASE_URI: string = process.env.DATABASE_URI || '';
 
-const APP: Express = express();
+const app: Express = express();
+
+// V1 API routes
+import AuthorizedUsersV1Route from './routes/api-v1/AuthorizedUsersRoute';
+
+app.use(function (req: Request, res: Response, next: NextFunction) {
+    logEvent(NODE_ENV, `${req.method}\t${req.headers.origin}\t${req.url}`);
+    next();
+});
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api/v1', AuthorizedUsersV1Route);
 
 mongoose
     .connect(DATABASE_URI)
     .then(function (): void {
-        APP.listen(PORT, function (): void {});
+        app.listen(PORT, function (): void {
+            logEvent(NODE_ENV, `Listening on PORT: ${PORT}`);
+        });
     })
     .catch(function (err: Error): void {
         console.log(err.message);
