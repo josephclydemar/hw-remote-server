9f08123c9d980c53dd6e676b8887b0039db1d749
Joseph Clyde Mar <clydemar234@gmail.com>
2024-04-17 00:24:21 +0800

    feat: Add profileImage property to AuthorizedUser model

--- src/models/AuthorizedUserModel.ts
+++ src/models/AuthorizedUserModel.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const AuthorizedUserSchema = new mongoose.Schema(
     {
+        profileImage: { type: String, required: true },
         name: { type: String, required: true },
     },
     { timestamps: true },
