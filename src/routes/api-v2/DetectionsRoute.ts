d2a09c83151d7665a394c1bec64190ba8e2a5406
Joseph Clyde Mar <clydemar234@gmail.com>
2024-05-09 02:55:52 +0800

    feat: Add a version2 api route for /detections

---
+++ src/routes/api-v2/DetectionsRoute.ts
@@ -0,0 +1,10 @@
+import express, { Router } from 'express';
+
+import DetectionsController from '../../controllers/DetectionController';
+
+
+const DetectionsV2Router: Router = express.Router();
+
+DetectionsV2Router.route('/detections').patch(DetectionsController.getSpecificDetections);
+
+export default DetectionsV2Router;
